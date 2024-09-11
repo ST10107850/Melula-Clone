4a01e991e0498b75eb1785bad7df486d17cae743
ST10107850 <ST10107850@rcconnect.edu.za>
2024-09-11 09:08:37 +0200

    Adding AOS

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,19 @@ import {  BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./Layout/MainLayout";
 import { Shop } from "./Pages/Shop";
 import { AboutPage } from "./Pages/AboutPage";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
-// import React from 'react'
+
 
  const App = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    })
+  })
   return (
     <div className="w-full h-full overflow-x-hidden" >
       <Router>
--- src/components/TwoPics.jsx
+++ src/components/TwoPics.jsx
@@ -2,20 +2,26 @@
 
 export const TwoPics = () => {
   return (
-    <div className="flex md:flex-row flex-col ">
+    <div className="flex md:flex-row flex-col" data-aos="fade-up">
+      <div className="w-full h-[80vh]">
+        <a href="/shop">
+          <img
+            className="w-full h-full transition-transform"
+            src="./27.jpg"
+            alt=""
+          />
+        </a>
+      </div>
 
       <div className="w-full h-[80vh]">
         <a href="/shop">
-         <img className="w-full h-full transition-transform" src="./27.jpg" alt="" />
-         </a>
+          <img
+            className="w-full h-full  transition-transform"
+            src="./28.jpg"
+            alt=""
+          />
+        </a>
       </div>
-        
-      <div className="w-full h-[80vh]">
-      <a href="/shop">
-         <img className="w-full h-full  transition-transform" src="./28.jpg" alt="" />
-         </a>
-      </div>
-
     </div>
-  )
-}
+  );
+};
